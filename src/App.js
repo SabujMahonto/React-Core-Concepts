a1f7a40c27ee47c4a3ca3971f22711284696773f
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-16 12:20:24 +0600

    react component how to use daynamic data concept Done

--- src/App.js
+++ src/App.js
@@ -14,8 +14,20 @@ const singerStyle ={
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
+      <Parson name= "Shakim al Hasan" job = "crickter"></Parson>
+      <Parson name= "Omorsani" job = "Nayok"></Parson>
+      <Parson name= "Mousumi" job = "Nayka"></Parson>
+     
+      <h1>Friend list</h1>
+      <Friend name = "Shohag Vai" age ="33" status ="married"></Friend>
+      <Friend></Friend>
+      
+
+
+
+
+
+      {/* <header className="App-header">
       <div className="container">
          <h1>Hello World</h1>
          <h4>I am here</h4>
@@ -23,9 +35,32 @@ function App() {
          <p style={singerStyle}>Name:{singer.name} {singer.job}</p>
          <p style={{color:'tomato', backgroundColor:'white'}}>Name:{singer2.name} {singer2.job}</p>
       </div>
-      </header>
+      </header> */}
     </div>
   );
+}
+
+function Parson(props){
+  return (
+    <div className='person'>
+     <div className='info'> 
+     <h2>Name:{props.name}</h2>
+      <p>Professors: {props.job}</p>
+      </div>
+    
+    </div>
+  )
+}
+
+
+function Friend(props){
+  return(
+    <div className='info'>
+      <h1>Name: {props.name}</h1>
+      <h6>Age: {props.age}</h6>
+      <h6>Status: {props.status}</h6>
+    </div>
+  )
 }
 
 export default App;
