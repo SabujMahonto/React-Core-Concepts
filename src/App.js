bcf2192281ade2bede6ba273e00f591fb532b136
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-16 11:08:16 +0600

    how to css style in react concept clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+const num = 3333;
+const singer = {name: 'Dr.Mahfouz', job:'singer'}
+const singer2 = {name: 'Eva Ramadan', job:'singer'}
+
+
+// css style 
+const singerStyle ={
+  color : 'green',
+  backgroundColor :'white',
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-    <h1>Hello World</h1>
+      <div className="container">
+         <h1>Hello World</h1>
+         <h4>I am here</h4>
+         <p>Number:{3333+num}</p>
+         <p style={singerStyle}>Name:{singer.name} {singer.job}</p>
+         <p style={{color:'tomato', backgroundColor:'white'}}>Name:{singer2.name} {singer2.job}</p>
+      </div>
       </header>
     </div>
   );
